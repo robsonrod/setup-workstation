665e77a3c10904bd9de0ff6a2144209b3a591f77
Robson Rodrigues <robsonrod@gmail.com>
2022-11-14 18:51:33 -0300

    Building script to configure my workstation

---
+++ install.sh
@@ -0,0 +1,40 @@
+#!/bin/env bash
+
+install_needed() {
+    sudo add-apt-repository ppa:kelleyk/emacs -y
+    sudo apt-get update && sudo apt-get -qq install ansible unzip git -y 
+}
+
+configure() {
+    install_needed
+    ansible-playbook ubuntu-config.yaml --ask-become-pass $1
+}
+
+usage() {
+    printf "\nUsage:\n"
+    echo " -f full install and configure"
+    echo " -c check configuration"
+}
+
+if [ "$#" -eq 0 ]; then 
+    usage; 
+    exit 1; 
+fi
+
+main() {
+
+    case $1 in
+        -c)
+           configure "--check"
+            ;;
+        -f)
+           configure
+            ;;
+        -h)
+            usage
+            ;;
+        *) usase ; exit 1;
+    esac
+}
+
+main "$@"
